c97e8e8a0518d08e0f578f4a177af8f3c7fd0455
asa-leholland <asaleholland@gmail.com>
2023-09-30 18:16:42 -0700

    feat: report pre and post

--- output.sh
+++ output.sh
@@ -21,6 +21,7 @@ fi
 if [ "$pre_stash_deletions" == "" ]; then
   pre_stash_deletions=0
 fi
+printf "Uncommitted:    𝜟=%d (+%d/-%d)\n" "$((pre_stash_insertions + pre_stash_deletions))" "$pre_stash_insertions" "$pre_stash_deletions"
 
 function quiet_stash() {
   git stash push --keep-index --quiet --message "" --include-untracked
@@ -36,35 +37,31 @@ function quiet_checkout_target_branch() {
 quiet_checkout_target_branch
 
 # Calculate the total insertions and deletions in the commit history
-total_insertions=0
-total_deletions=0
+committed_insertions=0
+committed_deletions=0
 
 while read -r line; do
   insertions=$(echo "$line" | awk '{print $1}')
   deletions=$(echo "$line" | awk '{print $2}')
-  total_insertions=$((total_insertions + insertions))
-  total_deletions=$((total_deletions + deletions))
+  committed_insertions=$((committed_insertions + insertions))
+  committed_deletions=$((committed_deletions + deletions))
 done < <(git log --shortstat --oneline "$target_branch..$current_branch" | grep -E "([0-9]+ insertions\(\+\))|([0-9]+ deletions\(-\))")
+
+printf "Committed:      𝜟=%d (+%d/-%d)\n" "$((committed_insertions + committed_deletions))" "$committed_insertions" "$committed_deletions"
 
 # Get the total insertions and deletions from the last lines of `git diff --shortstat`
 shortstat_output=$(git diff --shortstat 2>/dev/null)
 insertions=$(echo "$shortstat_output" | tail -n 1 | awk '{print $4}')
 deletions=$(echo "$shortstat_output" | tail -n 1 | awk '{print $6}')
 
-echo $total_deletions
-echo $pre_stash_deletions
-echo $total_insertions
-echo $pre_stash_insertions
-
-# Add the uncommitted changes (both before and after stashing) to the total
-total_insertions=$((total_insertions + pre_stash_insertions))
-total_deletions=$((total_deletions + pre_stash_deletions))
+total_insertions=$((committed_insertions + pre_stash_insertions))
+total_deletions=$((committed_deletions + pre_stash_deletions))
 
 # Print the result in the desired format
 if ((total_insertions + total_deletions > 0)); then
-  printf "𝜟=%d (+%d/-%d)\n" "$((total_insertions + total_deletions))" "$total_insertions" "$total_deletions"
+  printf "All Changes:    𝜟=%d (+%d/-%d)\n" "$((total_insertions + total_deletions))" "$total_insertions" "$total_deletions"
 else
-  printf "𝜟=0 (+0/-0)\n"
+  printf "All Changes:    𝜟=0 (+0/-0)\n"
 fi
 
 # Return to the original branch and unstash changes
